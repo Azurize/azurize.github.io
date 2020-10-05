c88f937973a18725e45681004ecc65485b28ab3d
David Grabosky <46197517+Azurize@users.noreply.github.com>
2020-10-05 15:46:29 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Landing Page for Prime Trust Postman Collection
---
+++ js/ptpost.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#3EFF15',
+    lineColor: '#BE00FE',
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
+}, false);
+
+
+/*
+// jQuery plugin example:
+$(document).ready(function() {
+  $('#particles').particleground({
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+});
+*/
